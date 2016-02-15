e3836705d654a83587950529d736b1dd767304d1
Matthias Dörfelt <hi@mokafolio.de>
2016-02-15 15:51:35 -0800

    replaced jsoncpp with sajson

--- Scrub/JSON/JSONSerializer.cpp
+++ Scrub/JSON/JSONSerializer.cpp
@@ -1,76 +1,74 @@
 #include <Scrub/JSON/JSONSerializer.hpp>
-#include <json/json.h>
+#include <Scrub/JSON/sajson.h>
 
 namespace scrub
 {
     namespace json
     {
-        static String JSONValueToString(const Json::Value & _val, Allocator & _alloc)
+        static String JSONValueToString(const sajson::value & _val, Allocator & _alloc)
         {
-            if (_val.isString())
+            if (_val.get_type() == sajson::TYPE_STRING)
             {
-                return String(_val.asString().c_str(), _alloc);
+                return String(_val.as_string().c_str(), _alloc);
             }
-            else if (_val.isBool())
+            else if (_val.get_type() == sajson::TYPE_TRUE)
             {
-                if (_val.asBool() == true)
-                    return String("true", _alloc);
-                else
-                    return String("false", _alloc);
+                return String("true", _alloc);
             }
-            else if (_val.isInt())
+            else if (_val.get_type() == sajson::TYPE_FALSE)
             {
-                return toString(_val.asInt(), _alloc);
+                return String("false", _alloc);
             }
-            else if (_val.isUInt())
+            else if (_val.get_type() == sajson::TYPE_INTEGER)
             {
-                return toString(_val.asUInt(), _alloc);
+                return toString(_val.get_integer_value(), _alloc);
             }
-            else if (_val.isDouble())
+            else if (_val.get_type() == sajson::TYPE_DOUBLE)
             {
-                return toString(_val.asDouble(), _alloc);
+                return toString(_val.get_double_value(), _alloc);
             }
 
             return String("", _alloc);
         }
 
-        static void parseJSONObject(const Json::Value & _node, Shrub & _treeNode);
+        static void parseJSONObject(const sajson::value & _node, Shrub & _treeNode);
 
-        static void parseJSONNode(const char * _name, const Json::Value & _node, Shrub & _treeNode)
+        static void parseJSONNode(const String & _name, const sajson::value & _node, Shrub & _treeNode)
         {
             Shrub child(_name, JSONValueToString(_node, _treeNode.allocator()), _treeNode.allocator());
-            if (_node.isObject())
+            if (_node.get_type() == sajson::TYPE_OBJECT)
             {
                 parseJSONObject(_node, child);
             }
-            else
+            else if (_node.get_type() == sajson::TYPE_ARRAY)
             {
-                for (Size i = 0; i < _node.size(); ++i)
+
+                for (Size i = 0; i < _node.get_length(); ++i)
                 {
-                    parseJSONNode("", _node.get((UInt32)i, Json::Value()), child);
+                    parseJSONNode("", _node.get_array_element(i), child);
                 }
             }
             _treeNode.append(move(child));
         }
 
-        static void parseJSONObject(const Json::Value & _node, Shrub & _treeNode)
+        static void parseJSONObject(const sajson::value & _node, Shrub & _treeNode)
         {
-            for (Size i = 0; i < _node.getMemberNames().size(); ++i)
+            STICK_ASSERT(_node.get_type() == sajson::TYPE_OBJECT);
+            for (Size i = 0; i < _node.get_length(); ++i)
             {
-                parseJSONNode(_node.getMemberNames()[i].c_str(), _node.get(_node.getMemberNames()[i], Json::Value()), _treeNode);
+                const sajson::string & str = _node.get_object_key(i);
+                parseJSONNode(String(str.data(), str.data() + str.length()), _node.get_object_value(i), _treeNode);
             }
         }
 
         ShrubResult parseJSON(const String & _json, Allocator & _alloc)
         {
-            Json::Value root;
-            Json::Reader reader;
-            bool parsingSuccessful = reader.parse(_json.cString(), root);
-            if (!parsingSuccessful)
+            const sajson::document & document = sajson::parse(sajson::literal(_json.cString()));
+            if (!document.is_valid())
             {
-                return Error(ec::ParseFailed, String::concat("Failed to parse JSON: ", reader.getFormatedErrorMessages().c_str()), STICK_FILE, STICK_LINE);
+                return Error(ec::ParseFailed, String::concat("Failed to parse JSON: ", document.get_error_message().c_str()), STICK_FILE, STICK_LINE);
             }
-
+            const sajson::value & root = document.get_root();
             Shrub ret(_alloc);
             parseJSONObject(root, ret);
             return ret;
